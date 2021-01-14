53446ff78c278b92d436196ae813a0cf7d0a9c49
Pit Jost <s8pijost@stud.uni-saarland.de>
2021-01-14 17:01:22 +0100

    Revised script

--- hpg_analysis/dom_clobbering/main.py
+++ hpg_analysis/dom_clobbering/main.py
@@ -8,14 +8,17 @@ if __name__ == '__main__':
     with database.session() as session:
         with session.begin_transaction() as transaction:
             results = transaction.run('''
-                MATCH   (call_expr {Type: "CallExpression"})-[:AST_parentOf {RelationType: "callee"}]->(callee),
+                MATCH   (expr_stmt)-[:AST_parentOf {RelationType: "expression"}]->(call_expr {Type: "CallExpression"})-[:AST_parentOf {RelationType: "callee"}]->(callee),
                         (call_expr)-[:AST_parentOf {RelationType: "arguments"}]->(call_arg),
                         (callee)-[:AST_parentOf*2]->({Type: "Identifier", Code: "document"}),
                         (callee)-[:AST_parentOf]->({Type: "Identifier", Code: "appendChild"})
-                RETURN  call_expr, call_arg;
+                RETURN  expr_stmt, call_arg;
             ''')
             for result in results:
-                call_expr = result['call_expr']
+                expr_stmt = result['expr_stmt']
                 call_arg = result['call_arg']
-                slices = data_flow._get_varname_value_from_context(transaction, call_arg['Code'], call_expr)
-                print(repr(slices))
+                slices = data_flow._get_varname_value_from_context(transaction, call_arg['Code'], expr_stmt)
+                for i in range(0, len(slices)):
+                    current_slice = slices[i]
+                    if 'window.' in current_slice[0]:
+                        print('DOM clobbering source found in document.*.appendChild() sink: %s (%s)' % (current_slice[0],str(current_slice[3])))
