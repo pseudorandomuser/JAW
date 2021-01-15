54a016c74fb18998ad16c7954e32319fc7622139
Pit Jost <s8pijost@stud.uni-saarland.de>
2021-01-15 14:55:21 +0100

    Include reachability check

--- hpg_analysis/dom_clobbering/main.py
+++ hpg_analysis/dom_clobbering/main.py
@@ -1,25 +1,28 @@
 import constants
 
 from neo4j import GraphDatabase
-from hpg_analysis.general import data_flow
+
+from hpg_analysis.general.control_flow import do_reachability_analysis
+from hpg_analysis.general.data_flow import _get_varname_value_from_context
+
+def get_document_appendchild_sinks(transaction):
+    query = '''
+        MATCH   (expr_node {Type: "ExpressionStatement"})-[:AST_parentOf {RelationType: "expression"}]
+                    ->(call_expr {Type: "CallExpression"})-[:AST_parentOf {RelationType: "callee"}]->(callee),
+                (call_expr)-[:AST_parentOf {RelationType: "arguments"}]->(args_node),
+                (callee)-[:AST_parentOf*2]->({Type: "Identifier", Code: "document"}),
+                (callee)-[:AST_parentOf]->({Type: "Identifier", Code: "appendChild"})
+        RETURN  expr_node, args_node;
+    '''
+    return [(result['expr_node'], result['args_node']) for result in transaction.run(query)]
 
 if __name__ == '__main__':
     database = GraphDatabase.driver(constants.NEO4J_CONN_STRING, auth=(constants.NEO4J_USER, constants.NEO4J_PASS))
     with database.session() as session:
         with session.begin_transaction() as transaction:
-            results = transaction.run('''
-                MATCH   (expr_stmt)-[:AST_parentOf {RelationType: "expression"}]->(call_expr {Type: "CallExpression"})-[:AST_parentOf {RelationType: "callee"}]->(callee),
-                        (call_expr)-[:AST_parentOf {RelationType: "arguments"}]->(call_arg),
-                        (callee)-[:AST_parentOf*2]->({Type: "Identifier", Code: "document"}),
-                        (callee)-[:AST_parentOf]->({Type: "Identifier", Code: "appendChild"})
-                RETURN  expr_stmt, call_arg;
-            ''')
-            for result in results:
-                expr_stmt = result['expr_stmt']
-                call_arg = result['call_arg']
-                slices = data_flow._get_varname_value_from_context(transaction, call_arg['Code'], expr_stmt)
-                for i in range(0, len(slices)):
-                    current_slice = slices[i]
-                    print('%d.\t%s' % (i, repr(current_slice)))
-                    '''if 'window.' in current_slice[0]:
-                        print('DOM clobbering source found in document.*.appendChild() sink: %s (%s)' % (current_slice[0],str(current_slice[3])))'''
+            for expr_node, args_node in get_document_appendchild_sinks(transaction):
+                if do_reachability_analysis(transaction, expr_node.id) == 'unreachable':
+                    print('Sink node with ID=%d is unreachable.' % expr_node.id); continue
+                for slice in _get_varname_value_from_context(transaction, args_node['Code'], expr_node):
+                    if 'window.' in slice[0]:
+                        print('DOM clobbering source found in document.*.appendChild() sink: %s (%s)' % (slice[0], str(slice[3])))
