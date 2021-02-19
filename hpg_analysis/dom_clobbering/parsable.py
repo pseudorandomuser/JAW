d1956c7a4cb3f3ad6abd4d4a8d5ba80ad0458a56
Pit Jost <s8pijost@stud.uni-saarland.de>
2021-02-19 16:47:43 +0100

    Improved parsing script

--- hpg_analysis/dom_clobbering/parsable.py
+++ hpg_analysis/dom_clobbering/parsable.py
@@ -6,8 +6,8 @@ import math
 import subprocess
 
 
-SITE_ID_MIN = 1
-SITE_ID_MAX = 1
+SITE_ID_MIN = 11
+SITE_ID_MAX = 15
 REQUIRED_VALID = 1
 
 PROJECT_ROOT = os.path.join(os.path.dirname(sys.argv[0]), f'..{os.path.sep}..')
@@ -41,6 +41,11 @@ if __name__ == '__main__':
         print(f'\nAttempting to parse JavaScript from URLs of site with ID {site_id}... ', flush=True)
 
         site_path = os.path.join(CLOBBER_DATA, str(site_id))
+
+        if not os.path.isdir(site_path):
+            print(f'Site with ID {site_id} does not exist, skipping...', end='', flush=True)
+            continue
+
         site_urls = [ dir for dir in os.listdir(site_path) if os.path.isdir(os.path.join(site_path, dir)) ]
         site_urls_count = len(site_urls)
 
@@ -50,15 +55,17 @@ if __name__ == '__main__':
 
             for current_index in range(0, site_urls_count):
 
-                progress_bar = get_progress_bar(current_index + 1, site_urls_count, width=40)
-                print(f'{progress_bar} ({current_index + 1}/{site_urls_count})', end='\r', flush=True)
-
                 url_hash = site_urls[current_index]
                 url_path = os.path.join(site_path, url_hash)
                 url_prog = os.path.join(url_path, 'js_program.js')
 
                 if parse_js(url_prog):
                     parsable_urls.append(url_hash)
+
+                progress_bar = get_progress_bar(current_index + 1, site_urls_count, width=40)
+                valid_count = len(parsable_urls)
+                invalid_count = current_index + 1 - valid_count
+                print(f'{progress_bar} ({current_index + 1}/{site_urls_count}) ({valid_count} OK, {invalid_count} FAIL)', end='\r', flush=True)
 
         except KeyboardInterrupt:
             parsing_interrupted = True
