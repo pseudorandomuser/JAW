5903e9c058aa4fee83baf6b2baac4e4adbfa742f
Pit Jost <s8pijost@stud.uni-saarland.de>
2021-03-02 14:38:40 +0100

    Added script to run dockerized analysis

---
+++ hpg_analysis/dom_clobbering/batch_analyze_dockerized.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JAW_ROOT="$(dirname "$0")/../.."
+SITE_ID=$1
+
+docker run --name JAW_$SITE_ID \
+	--env SITE_ID=$SITE_ID \
+	-v $JAW_ROOT/hpg_analysis/dom_clobbering/reports:/reports \
+	-v $JAW_ROOT/hpg_construction/outputs:/outputs \
+	v8bigblock/jaw_analysis
